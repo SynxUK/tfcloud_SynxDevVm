bddba4beb8af958b606646a67262d592515e8818
StuartD <stuart.dryden75@gmail.com>
2022-07-07 09:00:14 +0100

    provisioner fixed

--- Powershell/provision.tpl.ps1
+++ Powershell/provision.tpl.ps1
@@ -1,8 +1,5 @@
-# Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-# Invoke-Expression -Command 'choco install -y powershell-core git terraform vscode googlechrome'
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+Invoke-Expression -Command 'choco install -y powershell-core git terraform vscode googlechrome'
 $SshKey = '${SshKey}'
-New-Item -ItemType Directory -Path $HOME -Name '.ssh'
-$SshKey | set-content -Path "$HOME\.ssh\id_rsa"
-$SshKey | set-content -Path "c:\id_rsa"
-New-Item -ItemType Directory -Path "c:\" -Name "test"
-$SshKey | set-content -Path "c:\test\id_rsa"
+New-Item -ItemType Directory -Path "C:\Users\stuart" -Name '.ssh'
+$SshKey | set-content -Path "C:\Users\stuart\.ssh\id_rsa"
